2d7d707b37760e1a90d5b63f6f055eb479e6aee7
jplzhan <100379221+jplzhan@users.noreply.github.com>
2023-03-15 10:19:36 -0700

    Update install_mamba.sh

--- install_mamba.sh
+++ install_mamba.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+
+# Take 1 string argument as a command to run, which it echos before running
+function runCmd() { echo $1; $1; }
+
 # Installs mambaforge locally without polluting the global environment
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh .
-bash Mambaforge-Linux-x86_64.sh
-rm Mambaforge-Linux-x86_64.sh
+runCmd "wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh ."
+runCmd "bash Mambaforge-Linux-x86_64.sh"
+runCmd "rm Mambaforge-Linux-x86_64.sh"
