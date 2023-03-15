fc335048f28bc496a4e9a1153af7ffe725f2e003
jplzhan <100379221+jplzhan@users.noreply.github.com>
2023-03-15 10:20:18 -0700

    Update create_mamba_env.sh

--- create_mamba_env.sh
+++ create_mamba_env.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+
+# Take 1 string argument as a command to run, which it echos before running
+function runCmd() { echo $1; $1; }
+
 # Creates a mamba environment in the current environment with the given python version
-PWD=$(pwd)
-mamba create --prefix $PWD/.conda python=$1
-mamba activate $PWD/.conda/
+runCmd "PWD=$(pwd)"
+runCmd "mamba create --prefix $PWD/.conda python=$1"
+runCmd "mamba activate $PWD/.conda/"
