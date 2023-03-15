a71e023d56b4b38fe423fc0789eb92cfde31ffa1
jplzhan <100379221+jplzhan@users.noreply.github.com>
2023-03-15 10:18:57 -0700

    Update install_codedark.sh

--- install_codedark.sh
+++ install_codedark.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # Installs vim codedark for this user
 basedir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp $basedir/.vimrc ~/
+
+# Take 1 string argument as a command to run, which it echos before running
+function runCmd() { echo $1; $1; }
+
+runCmd "curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+runCmd "cp $basedir/.vimrc ~/"
 echo "\n\n<<<REMEMBER TO CALL :PlugInstall INSIDE VIM!!!!>>>\n\n"
