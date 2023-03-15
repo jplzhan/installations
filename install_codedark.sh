c49c8fbcf1cc628d3604bcc56b3584113c3fb3d8
jplzhan <100379221+jplzhan@users.noreply.github.com>
2023-03-15 10:18:10 -0700

    Update install_codedark.sh

--- install_codedark.sh
+++ install_codedark.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Installs vim codedark for this user
-basedir=$(dirname "$(readlink -f "$0")")
+basedir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp $basedir/.vimrc ~/
 echo "\n\n<<<REMEMBER TO CALL :PlugInstall INSIDE VIM!!!!>>>\n\n"
