884af0bed5c57bc630cc69e176f4fd966d039f3b
jplzhan <100379221+jplzhan@users.noreply.github.com>
2024-04-04 16:29:20 -0700

    Update install_mamba.sh
    set -x instead of runCmd

--- install_mamba.sh
+++ install_mamba.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# Take 1 string argument as a command to run, which it echos before running
-function runCmd() { echo $1; $1; }
+set -x
 
 # Installs mambaforge locally without polluting the global environment
-runCmd "wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh ."
-runCmd "bash Mambaforge-Linux-x86_64.sh"
-runCmd "rm Mambaforge-Linux-x86_64.sh"
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh .
+bash Mambaforge-Linux-x86_64.sh
+rm Mambaforge-Linux-x86_64.sh
