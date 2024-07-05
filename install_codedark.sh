86b1b3c09d91f2e5b32d30346710140f3be6a9ac
max.zhan@jpl.nasa.gov <Max Zhan>
2024-07-05 15:27:56 +0000

    Improved codedark install script to not requre manual interaction.

--- install_codedark.sh
+++ install_codedark.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # Installs vim codedark for this user
+set -x
 basedir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # Take 1 string argument as a command to run, which it echos before running
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp $basedir/.vimrc ~/
-printf "\n\n<<<REMEMBER TO CALL :PlugInstall INSIDE VIM!!!!>>>\n\n"
+cp $basedir/.plug_install ~/.vimrc
+vim -y +PlugInstall +qall > /dev/null
+cp $basedir/.vimrc ~/.vimrc
